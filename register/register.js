998098af316eabbb7840af852ae5b3baa7b1f223
anandiooooooooooooooooooooooooooooooooo <anandhio123@gmail.com>
2024-07-12 14:22:05 +0700

    baru lagi

---
+++ register/register.js
@@ -0,0 +1,16 @@
+var a;
+function pass()
+{
+    if(a==1)
+    {
+        document.getElementById('password').type='password';
+        document.getElementById('pass-icon');
+        a=0;
+    }
+    else
+    {
+        document.getElementById('password').type='text';
+        document.getElementById('pass-icon'); 
+        a=1;
+    }
+}
